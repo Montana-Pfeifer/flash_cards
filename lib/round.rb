befff807d3f34d15527c5dfd032393d1192a2929
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-08-25 19:12:02 -0600

    added take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,24 @@
 class Round
-    attr_reader
+    attr_reader :deck, :turn, :current_card_index
 
     def initialize(deck)
-        @deck = deck
-        @turn =[]
-        @current_card_index = 0
+            @deck = deck
+            @turn =[]
+            @current_card_index = 0
     end
-def current_card
-    @deck.cards[current_card_index]
-end
 
+    def current_card
+            @deck.cards[@current_card_index]
+    end
 
+    def take_turn(guess)
+            current_card = self.current_card
+            turn = Turn.new(guess, current_card)
+            @turns << turn
+            @current_card_index += 1 if @current_card_index < @deck.count - 1
+            turn
+    end
+
+    def
 
 end

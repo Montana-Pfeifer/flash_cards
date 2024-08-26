29c7ea5c10b4f4d138c835b80308d112002194c7
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-08-25 20:38:42 -0600

    add 2nd test / pass

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,9 @@
 class Round
-    attr_reader :deck, :turn, :current_card_index
+    attr_reader :deck, :turns, :current_card_index
 
     def initialize(deck)
             @deck = deck
-            @turn =[]
+            @turns =[]
             @current_card_index = 0
     end
 
@@ -19,6 +19,22 @@ class Round
             turn
     end
 
-    def
+    def number_correct
+            @turns.count { |turn| turn.correct? }
+    end
+
+    def number_correct_by_category(category)
+             @turns.count { |turn| turn.correct? && turn.card.category == category}
+    end
+
+    def percent_correct
+            (number_correct.to_f / @turns.count) * 100
+    end
+
+    def percent_correct_by_category(category)
+        total_in_category + @turns.count { |turn| turn.card.category == category }
+        correct_in_category = number_correct_by_category(category)
+        (correct_in_category.to_f / total_in_category) * 100
+    end
 
 end
